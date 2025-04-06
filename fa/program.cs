afdaa3e4d8747d4842603a276990eae3cf2132aa
Danila Ikryanikov <danila.ikryanikov@mail.ru>
2025-04-07 02:10:50 +0300

    Добавлены классы FA1, FA2, FA3

--- fa/program.cs
+++ fa/program.cs
@@ -13,30 +13,258 @@ namespace fans
     public bool IsAcceptState;
   }
 
-
   public class FA1
   {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
+        /// <summary>
+        /// Состояние "A"
+        /// </summary>
+        public static State stateA = new State()
+        {
+            Name = "A",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        /// <summary>
+        /// Состояние "B"
+        /// </summary>
+        public State stateB = new State()
+        {
+            Name = "B",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        /// <summary>
+        /// Состояние "C"
+        /// </summary>
+        public State stateC = new State()
+        {
+            Name = "C",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        /// <summary>
+        /// Состояние "D"
+        /// </summary>
+        public State stateD = new State()
+        {
+            Name = "D",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        /// <summary>
+        /// Состояние "E"
+        /// </summary>
+        public State stateE = new State()
+        {
+            Name = "E",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+
+
+        State InitialState;
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public FA1()
+        {
+            InitialState = stateA;
+
+            stateA.Transitions['0'] = stateC;
+            stateA.Transitions['1'] = stateB;
+
+            stateB.Transitions['0'] = stateD;
+            stateB.Transitions['1'] = stateB;
+
+            stateC.Transitions['0'] = stateE;
+            stateC.Transitions['1'] = stateD;
+
+            stateD.Transitions['0'] = stateE;
+            stateD.Transitions['1'] = stateD;
+
+            stateE.Transitions['0'] = stateE;
+            stateE.Transitions['1'] = stateE;
+        }
+
+        /// <summary>
+        /// Запустить конечный автомат
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool? Run(IEnumerable<char> s)
+        {
+            State current = InitialState;
+            foreach (var item in s)
+            {
+                current = current.Transitions[item];
+                if (current == null)
+                {
+                    return null;
+                }
+            }
+            return current.IsAcceptState;
+        }
     }
-  }
 
   public class FA2
   {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
-    }
-  }
+        /// <summary>
+        /// Состояние "A"
+        /// </summary>
+        public static State stateA = new State()
+        {
+            Name = "a",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        /// <summary>
+        /// Состояние "B"
+        /// </summary>
+        public State stateB = new State()
+        {
+            Name = "b",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        /// <summary>
+        /// Состояние "C"
+        /// </summary>
+        public State stateC = new State()
+        {
+            Name = "c",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        /// <summary>
+        /// Состояние "D"
+        /// </summary>
+        public State stateD = new State()
+        {
+            Name = "d",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        State InitialState;
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public FA2()
+        {
+            InitialState = stateA;
+
+            stateA.Transitions['0'] = stateC;
+            stateA.Transitions['1'] = stateB;
+
+            stateB.Transitions['0'] = stateD;
+            stateB.Transitions['1'] = stateA;
+
+            stateC.Transitions['0'] = stateA;
+            stateC.Transitions['1'] = stateD;
+
+            stateD.Transitions['0'] = stateB;
+            stateD.Transitions['1'] = stateC;
+        }
+
+        /// <summary>
+        /// Запустить конечный автомат
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool? Run(IEnumerable<char> s)
+        {
+            State current = InitialState;
+            foreach (var item in s)
+            {
+                current = current.Transitions[item];
+                if (current == null)
+                {
+                    return null;
+                }
+            }
+            return current.IsAcceptState;
+        }
+   }
   
   public class FA3
   {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
-    }
-  }
+        /// <summary>
+        /// Состояние "A"
+        /// </summary>
+        public static State stateA = new State()
+        {
+            Name = "a",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        /// <summary>
+        /// Состояние "B"
+        /// </summary>
+        public State stateB = new State()
+        {
+            Name = "b",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        /// <summary>
+        /// Состояние "C"
+        /// </summary>
+        public State stateC = new State()
+        {
+            Name = "c",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        State InitialState;
+
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public FA3()
+        {
+            InitialState = stateA;
+
+            stateA.Transitions['0'] = stateA;
+            stateA.Transitions['1'] = stateB;
+
+            stateB.Transitions['0'] = stateA;
+            stateB.Transitions['1'] = stateC;
+
+            stateC.Transitions['0'] = stateC;
+            stateC.Transitions['1'] = stateC;
+        }
+
+        /// <summary>
+        /// Запустить конечный автомат
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool? Run(IEnumerable<char> s)
+        {
+            State current = InitialState;
+            foreach (var item in s)
+            {
+                current = current.Transitions[item];
+                if (current == null)
+                {
+                    return null;
+                }
+            }
+            return current.IsAcceptState;
+        }
+   }
 
   class Program
   {
